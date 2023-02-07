2ed831771e2f1db1d60f8711f5081a992b37f9ea
Zeyad Maher7 <Zeyadmaher638@gmail.com>
2023-02-07 12:00:28 +0200

    demo ml model detector for lemon

--- lib/modules/upload_screen.dart
+++ lib/modules/upload_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'dart:async';
-
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:defect_detection/Shared/components/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -17,7 +17,19 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadModel();
+    setState(() {
+
+    });
+  }
      final ImagePicker _picker = ImagePicker();
+  var decision="no result";
+  var percentage=0.0;
+  var check_done=false;
 
   Future pickImage()async{
  print("i entred pick");
@@ -33,6 +45,36 @@ class _UploadScreenState extends State<UploadScreen> {
     
     }
 
+     loadModel()async{
+       String? res = await Tflite.loadModel(
+           model: "assets/model_unquant.tflite",
+           labels: "assets/labels.txt",
+           numThreads: 1, // defaults to 1
+           isAsset: true, // defaults to true, set to false to load resources outside assets
+           useGpuDelegate: false // defaults to false, set to true to use GPU delegate
+       );
+
+     }
+     classifyImage()async{
+       var recognitions = await Tflite.runModelOnImage(
+           path: image!.path,   // required
+           imageMean: 127.5,   // defaults to 117.0
+           imageStd: 127.5,  // defaults to 1.0
+           numResults: 2,    // defaults to 5
+           threshold: 0.5,   // defaults to 0.1
+           asynch: true      // defaults to true
+       );
+
+       decision=recognitions![0]['label'];
+       percentage=recognitions[0]['confidence'];
+       decision=decision.substring(2);
+       print(decision);
+       print(percentage);
+       check_done=true;
+       setState(() {
+
+       });
+     }
   File? image;
   @override
   Widget build(BuildContext context) {
@@ -49,9 +91,9 @@ class _UploadScreenState extends State<UploadScreen> {
             children: [
               Text("Check your image")
               ,
-              SizedBox(height: 40,),
+              SizedBox(height: 20,),
               Container(
-                height: 108,
+                height: 70,
                 width: 219,
                 decoration: BoxDecoration(
                   color: Color(0xffd9d9d9),
@@ -64,14 +106,46 @@ class _UploadScreenState extends State<UploadScreen> {
                   ),
 
               ),
+          SizedBox(height: 30,)
+          ,
               image!=null?Image.file(image!,
-              width: 300,
-              height: 300,
+              width: 200,
+              height: 200,
               fit:BoxFit.cover,
-              
+
+
               ):FlutterLogo(
-                size: 250,
+                size: 100,
               ) ,
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: MaterialButton(onPressed: ()=>classifyImage() ,child: Text("check",style:TextStyle(color: Colors.white),),))
+            ,
+            SizedBox(height: 20,)
+            ,decision=="Defected"?Text("Defected Product !",style: TextStyle(
+                color: Colors.red,
+                fontSize: 30
+              ),):Text(decision,style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 30
+              ),
+              ),
+             check_done!=false? Row(
+               crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(child: Text("with Confidence level :")),
+                  Text(percentage.toString()),
+
+                ],
+              ):Row()
 
             ],
           ),
